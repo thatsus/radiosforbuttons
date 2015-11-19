5e14aae789037ef5d008f799fe0cf86b063115f8
Marshall Davis <mdavis@thatsus.com>
2015-11-19 13:58:23 -0500

    Change input names.
     Input names can have square brackets. This causes jQuery selectors
    to act up. This should repair that.

--- src/jquery.radiosforbuttons.js
+++ src/jquery.radiosforbuttons.js
@@ -1,8 +1,8 @@
-( function( $ ) {
+(function ($) {
   var colors = {
     white: '',
     blue: 'btn-primary',
-    primary: 'btn-primary', 
+    primary: 'btn-primary',
     lightblue: 'btn-info',
     info: 'btn-info',
     green: 'btn-success',
@@ -10,12 +10,12 @@
     yellow: 'btn-warning',
     warning: 'btn-warning',
     red: 'btn-danger',
-    danger: 'btn-danger',   
+    danger: 'btn-danger',
     black: 'btn-inverse',
     inverse: 'btn-inverse',
     link: 'btn-link',
-    inactive: 'btn-primary inactive-radios',
-  }
+    inactive: 'btn-primary inactive-radios'
+  };
 
   $.fn.radiosforbuttons = function (options) {
     var defaults = {
@@ -23,67 +23,67 @@
       color: this.data('button-color'),
       group: true,
       vertical: false,
-			autowidth: true,
-      margin: 0,
+      autowidth: true,
+      margin: 0
     };
-    var options = $.extend(defaults, options);
+    options = $.extend(defaults, options);
     options.margindirection = options.vertical ? 'margin-top' : 'margin-left';
-		if (options.margin) options.margin = options.margin - 1;
+    if (options.margin) options.margin = options.margin - 1;
     if (options.color in colors) options.buttonstyle = colors[options.color];
     if (options.buttonstyle in colors) options.buttonstyle = colors[options.buttonstyle];
 
     var div = this.eq(0);
     var name = div.find('input[type="radio"]').attr('name');
-    var trash = $('<div/ >').addClass('radiosforbuttons-'+name+'-trash').appendTo( div.closest('form') );
+    var trash = $('<div/ >').addClass('radiosforbuttons-' + name + '-trash').appendTo(div.closest('form'));
 
-    div.find('input[type="radio"]').each( function (k, v) {
-      var radio = $( this );
-      var label = $( 'label[for="'+radio.attr('id')+'"]' );
+    div.find('input[type="radio"]').each(function (k) {
+      var radio = $(this);
+      var label = $('label[for="' + radio.attr('id') + '"]');
 
       var color;
-      color = radio.data('button-color');
       color = radio.data('button-bootstrap-class');
-      if ( radio.data('button-color') in colors ) color = colors[radio.data('button-color')];
-      if ( radio.data('button-bootstrap-class') in colors ) color = colors[radio.data('button-bootstrap-class')];
-      $( '<button/ >' )
-        .addClass('btn')
-        .addClass( color ? color : options.buttonstyle )
-        .addClass('radiosforbuttons-'+name)
-				.addClass( radio.prop('checked') ? 'active' : '' )
-        .data('id', radio.attr('id'))
-        .val( radio.val() )
-        .html( label.html() )
-				.css('margin-right', 0)
-        .css(options.margindirection, k != 0 && options.margin != 0 ? options.margin : '')
-        .appendTo(div);
+      if (radio.data('button-color') in colors) color = colors[radio.data('button-color')];
+      if (radio.data('button-bootstrap-class') in colors) color = colors[radio.data('button-bootstrap-class')];
+      $('<button/ >')
+          .addClass('btn')
+          .addClass(color ? color : options.buttonstyle)
+          .addClass('radiosforbuttons-' + name)
+          .addClass(radio.prop('checked') ? 'active' : '')
+          .data('id', radio.attr('id'))
+          .val(radio.val())
+          .html(label.html())
+          .css('margin-right', 0)
+          .css(options.margindirection, k != 0 && options.margin != 0 ? options.margin : '')
+          .appendTo(div);
 
       radio.hide().appendTo(trash);
       label.remove();
-    })
+    });
 
-		div.addClass('radiosforbuttons-maindiv-'+name);
-		if (options.group) {
-		  div
-		    .addClass('btn-group')
-		    .addClass('btn-group-'+name);
-		  if (options.vertical) {
-				div.addClass('btn-group-vertical');
-				if (options.autowidth) {
-					var widths = [];
-					div.find('button').each( function () {
-						widths.push( $(this).outerWidth() )
-					}).css('width', Math.max.apply(Math, widths))
-				}
-			}
-		}
+    div.addClass('radiosforbuttons-maindiv-' + name);
+    if (options.group) {
+      div
+          .addClass('btn-group')
+          .addClass('btn-group-' + name);
+      if (options.vertical) {
+        div.addClass('btn-group-vertical');
+        if (options.autowidth) {
+          var widths = [];
+          div.find('button').each(function () {
+            widths.push($(this).outerWidth())
+          }).css('width', Math.max.apply(Math, widths))
+        }
+      }
+    }
 
-    $( '.radiosforbuttons-'+name ).click( function () {
-      $( '.radiosforbuttons-'+name+' input:radio[name="'+name+'"]' ).prop('checked', false);
-      $( '#'+$(this).data('id') ).prop('checked', true).trigger("change");
-      $( '.radiosforbuttons-'+name ).removeClass('active');
-      $( this ).addClass('active');
+    name = name.replace('[', '\\[').replace(']', '\\]');
+    $('.radiosforbuttons-' + name).click(function () {
+      $('.radiosforbuttons-' + name + ' input:radio[name="' + name + '"]').prop('checked', false);
+      $('#' + $(this).data('id')).prop('checked', true).trigger("change");
+      $('.radiosforbuttons-' + name).removeClass('active');
+      $(this).addClass('active');
       return false;
-    })
+    });
 
     return div;
   };
